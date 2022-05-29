8ccea2a2424c69216db3f3728c4e9df01c919679
Bruno Toledo <brunito_toledo@hotmail.com>
2022-05-28 22:20:50 -0300

    un poquito de css y html

---
+++ index.js
@@ -0,0 +1,10 @@
+var elemento = document.getElementById("title").innerHTML;
+console.log(elemento)
+
+function cargarPersonaje(){
+    var div = document.querySelector("div")
+    console.log(div)
+    
+    var elemento = document.getElementById("title").innerHTML;
+    console.log(elemento)
+}
